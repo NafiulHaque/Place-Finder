454d6bf32bc7c656ba635b7caa977855c1704016
Nafiul <nafiulhaque22@gmail.com>
2021-11-05 20:31:28 +0600

    add place form submit complete

--- src/shared/components/FormElements/Input.js
+++ src/shared/components/FormElements/Input.js
@@ -1,4 +1,4 @@
-import React, { useReducer } from "react";
+import React, { useEffect, useReducer } from "react";
 
 import { validate } from "../util/validators";
 import './Input.css';
@@ -31,6 +31,14 @@ const Input = props => {
         isTouched: false,
         isValid: false
     });
+
+    const { id, onInput } = props;
+    const { value, isValid } = inputState;
+
+    useEffect(() => {
+        onInput(id, value, isValid)
+
+    }, [id, value, isValid, onInput]);
 
     const changeHandler = event => {
         dispatch({
