47554d0183d5beb11190fd59beb8e113db222d05
Nafiul <nafiulhaque22@gmail.com>
2021-11-07 20:00:09 +0600

    delete button functionality added

--- src/places/components/PlaceItem.js
+++ src/places/components/PlaceItem.js
@@ -8,11 +8,26 @@ import './PlaceItem.css';
 
 const PlaceItem = props => {
     const [showMap, setShowMap] = useState(false);
+    const [showConfirmModal, setShowConfirmModal] = useState(false);
 
     const openMapHandler = () => setShowMap(true);
 
     const closeMapHandler = () => setShowMap(false);
 
+
+    const showDeleteWarningHandler = () => {
+        setShowConfirmModal(true);
+
+    }
+
+    const cancelDeleteHandler = () => {
+        setShowConfirmModal(false);
+    }
+
+    const confirmDeleteHandler = () => {
+        setShowConfirmModal(false);
+        console.log("Deleting......");
+    }
     return <React.Fragment>
         <Modal
             show={showMap}
@@ -25,6 +40,20 @@ const PlaceItem = props => {
             <div className="map-container">
                 <h2>THE MAP !</h2>
             </div>
+        </Modal>
+        <Modal
+            show={showConfirmModal}
+            onCancel={cancelDeleteHandler}
+            header="Are you sure?"
+            footerClass="place-item__modal-actions"
+            footer={
+                <React.Fragment>
+                    <Button inverse onClick={cancelDeleteHandler}>CANCEL</Button>
+                    <Button danger onClick={confirmDeleteHandler}>DELETE</Button>
+                </React.Fragment>
+            }
+        >
+            <p>Do you want to proceed and delete this Place?</p>
         </Modal>
         <li className="place-item">
             <Card className="place-item__content">
@@ -40,7 +69,7 @@ const PlaceItem = props => {
                 <div className="place-item__actions">
                     <Button inverse onClick={openMapHandler}>VIEW ON MAP</Button>
                     <Button to={`/places/${props.id}`}>EDIT</Button>
-                    <Button danger>DELETE</Button>
+                    <Button danger onClick={showDeleteWarningHandler}>DELETE</Button>
                 </div>
             </Card>
         </li>
