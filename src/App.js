7b3fd538d7f3dabcdbfa4715af08edb2b7746cde
Nafiul <nafiulhaque22@gmail.com>
2021-11-08 18:31:54 +0600

    auth page design

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import NewPlace from './places/pages/NewPlace';
 import UserPlaces from './places/pages/UserPlaces';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
 import UpdatePlace from './places/pages/UpdatePlace';
+import Auth from './places/pages/Auth';
 
 
 const App = () => {
@@ -26,6 +27,9 @@ const App = () => {
         </Route>
         <Route path="/places/:placeId">
           <UpdatePlace />
+        </Route>
+        <Route path="/auth">
+          <Auth />
         </Route>
         <Redirect to="/" />
       </Switch>
