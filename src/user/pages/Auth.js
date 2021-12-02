d64c47a3bceb06602b4f03e67f0cccd9862cd42b
Nafiul <nafiulhaque22@gmail.com>
2021-12-02 21:06:35 +0600

    loading and error msg handling

--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -5,6 +5,8 @@ import Button from "../../shared/components/FormElements/Button";
 import Input from "../../shared/components/FormElements/Input";
 import Card from "../../shared/components/UIElements/Card";
 import { useForm } from "../../shared/hooks/form-hook";
+import ErrorModal from "../../shared/components/UIElements/ErrorModal";
+import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
 import {
     VALIDATOR_EMAIL,
     VALIDATOR_MINLENGTH,
@@ -16,9 +18,9 @@ import './Auth.css';
 const Auth = () => {
 
     const auth = useContext(AuthContext);
-
     const [isLoginMode, setLoginMode] = useState(true);
-
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState();
 
     const [formState, inputHandler, setFormData] = useForm({
         email: {
@@ -61,6 +63,7 @@ const Auth = () => {
 
         } else {
             try {
+                setIsLoading(true);
                 const response = await fetch('http://localhost:5000/api/users/signup', {
                     method: 'POST',
                     headers: {
@@ -75,17 +78,20 @@ const Auth = () => {
 
                 const responseData = await response.json();
                 console.log(responseData);
+                setIsLoading(false);
+                auth.login();
 
             } catch (err) {
                 console.log(err);
+                setIsLoading(false);
+                setError(err.message || 'Something went wrong, plaease try again');
             }
         }
 
-
-
-        auth.login();
+        setIsLoading(false);
     }
     return <Card className="authentication">
+        {isLoading && <LoadingSpinner asOverlay />}
         <h2>Login Required</h2>
         <hr />
         <form onSubmit={authSubmitHandler}>
