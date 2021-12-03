70d3756b46eccb0550bb32e627a8b1e8d7308f51
Nafiul <nafiulhaque22@gmail.com>
2021-12-03 18:38:24 +0600

    F-B getting user

--- src/user/pages/User.js
+++ src/user/pages/User.js
@@ -1,17 +1,57 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
+import ErrorModal from "../../shared/components/UIElements/ErrorModal";
+import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
+
 import UserList from "../components/UserList";
 
 
 
 const Users = () => {
-    const USERS = [
-        {
-            id: 'u1',
-            name: 'Nafiul Haque',
-            image: 'https://firebasestorage.googleapis.com/v0/b/myapp-c3e74.appspot.com/o/82531761_171253510889533_5937920248739138889_n.jpg?alt=media&token=c17d0068-95ec-48df-bb40-2564675904ca',
-            places: 3
-        }]
-    return <UserList items={USERS} />
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState();
+    const [loadedUsers, setLoadedUsers] = useState();
+
+
+    useEffect(() => {
+        const sendRequest = async () => {
+            setIsLoading(true);
+            try {
+                const response = await fetch('http://localhost:5000/api/users');
+
+                const responseData = await response.json();
+
+                if (!response.ok) {
+                    throw new Error(responseData.message);
+                }
+
+
+                setLoadedUsers(responseData.users);
+
+            } catch (err) {
+
+                setError(err.message);
+            }
+            setIsLoading(false);
+        };
+        sendRequest();
+
+    }, []);
+
+    const errorHandler = () => {
+        setError(null);
+    }
+    return (
+        <React.Fragment>
+            <ErrorModal error={error} onClear={errorHandler} />
+            {isLoading && (
+                <div className="center">
+                    <LoadingSpinner />
+                </div>
+            )}
+            {!isLoading && loadedUsers && <UserList items={loadedUsers} />}
+        </React.Fragment>
+    );
+
 };
 
 export default Users;
